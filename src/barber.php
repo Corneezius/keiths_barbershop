b9fcb35b5ffc1562f0e55dd48c841a42527ea9df
Cornelius Range <Guest@Epicoduss-iMac-55.local>
2016-10-04 12:57:28 -0400

    add barber class

---
+++ src/barber.php
@@ -0,0 +1,98 @@
+<?php
+
+  class Barber
+  {
+    private $name;
+    private $barber_id;
+
+    function __construct($barber_name, $new_id = null)
+    {
+      $this->$name = $barber_name;
+      $this->$barber_id = $new_id;
+    }
+
+    function getId()
+    {
+      return $this->barber_id;
+    }
+
+    function setName($barber_name)
+    {
+      $this->name =$barber_name;
+    }
+
+    function getName()
+    {
+      return $this->name;
+    }
+
+    function save()
+    {
+        $GLOBALS['DB']->exec("INSERT INTO barbers (name) VALUES ('{$this->getName()}');");
+        $this->barber_id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    function getClients()
+    {
+      $clients = array();
+      $returned_clients = $GLOBALS['DB']->query("SELECT * FROM clients WHERE barber_id = {$this->getID()};")
+
+      foreach($returned_clients as $client)
+      {
+        $client_name = $client['name'];
+        $id = $client['id'];
+        $barber_id = $client['barber_id'];
+        $new_client = new Client($client_name, $barber_id, $id);
+        array_push($clients, $new_client);
+      }
+        return $clients;
+      }
+
+      function update($new_name)
+      {
+        $GLOBALS['DB']->exec("UPDATE barbers SET name = '{$new_name}' WHERE id = {$this->getId()}");
+        $this->setName($new_name);
+      }
+
+      static function getAll()
+      {
+        $returned_barbers = $GLOBALS["DB"]->query("SELECT * FROM barbers;");
+        $barbers = array();
+        foreach ($returned_barbers as $barber)
+        {
+            $name = $barber['name'];
+            $barber_id = $barber['id'];
+            $new_barber = new Barber($name, $barber_id);
+            array_push($barbers, $new_barber);
+        }
+
+        return $barbers;
+      }
+
+      function delete()
+      {
+        $GLOBALS['DB']->exec("DELETE FROM barbers WHERE id = {$this->getId()};");
+        $GLOBALS['DB']->exec("DELETE FROM clients WHERE barber_id = {$this->getId()};");
+      }
+
+      static function deleteAll()
+      {
+          $GLOBALS['DB']->exec("DELETE from barbers");
+      }
+
+      static function find($search_id)
+        {
+            $found_barber = null;
+            $barbers = Barber::getAll();
+            foreach($barbers as $barber)
+            {
+                $barber_id = $barber->getId();
+                if ($barber_id == $search_id)
+                {
+                    $found_barber = $barber;
+                }
+            }
+            return $found_barber;
+        }
+    }
+?>
